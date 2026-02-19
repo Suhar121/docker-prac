22afcfcce77626bc0da107c1358fb8f43105416d
Suhar Yaseen <suharyaseen36@gmail.com>
2026-02-19 08:31:35 +0530

    inital comment

---
+++ backend/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+app=FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message":"hello world"}
+
+@app.get("/items/{item_id}")
+def read_items(item_id:int):
+    return {"item_id":item_id}
